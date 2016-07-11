5c4b82730feef00c5ef5506bb18f6074571f28a1
marinacor1 <marinaspam1234@gmail.com>
2016-07-10 22:18:12 -0600

    Add all routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,25 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get '/auth/github', as: :github_login
+  get '/auth/github/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy', as: :logout
+  delete '/logout', to: 'sessions#destroy'
+  resources :users, only: [:show, :edit, :update]
+
+  namespace :english do
+    get '/description', to: 'description#index'
+    get '/contact', to: 'contact#index'
+  end
+
+  namespace :espanol do
+    get '/descripcion', to: 'description#index'
+    get '/contacto', to: 'contact#index'
+  end
+
+
+  namespace :admin do
+    get '/dashboard', to: 'users#show'
+    resources :users, only: [:update, :delete, :index]
+  end
 
 end
